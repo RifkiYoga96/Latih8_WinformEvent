8f0252d45e42fbc0932e88b4557408af978e6556
RifkiYoga96 <151479896+RifkiYoga96@users.noreply.github.com>
2024-07-04 10:51:15 +0700

    Add files via upload

---
+++ Latih8_WinformEvent/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Latih8_WinformEvent
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(23, 32);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(914, 308);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.Click += new System.EventHandler(this.dataGridView1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.comboBox2);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.dateTimePicker1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(41, 346);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(678, 178);
+            this.panel1.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(434, 105);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(200, 28);
+            this.comboBox2.TabIndex = 6;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(207, 105);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(200, 28);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(434, 19);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(200, 70);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Validated += new System.EventHandler(this.textBox3_Validated);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "dddd, dd-MM-yyyy";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(207, 63);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(221, 26);
+            this.dateTimePicker1.TabIndex = 3;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(207, 19);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(200, 26);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Validated += new System.EventHandler(this.textBox2_Validated);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(26, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(153, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(822, 358);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 41);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Load Data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(968, 533);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Latih8_WinformEvent/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.ObjectModel;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Latih8_WinformEvent
+{
+    public partial class Form1 : Form
+    {
+
+        private List<SiswaModel> listSiswa;
+        private BindingSource bindingSource;
+        public Form1()
+        {
+            InitializeComponent();
+            GenerateData();
+        }
+
+        public void GenerateData()
+        {
+            listSiswa = new List<SiswaModel>
+            {
+                new SiswaModel("16666","Rifki Yoga",new DateTime(2006,2,3),"Laki-laki","Jl.Suko Mbahmu No.7,Bantul","RPL"),
+                new SiswaModel("16668","Rifki Daffa",new DateTime(2006,2,3),"Laki-laki","Jl.Melati No.2 ,Bantul","TKJ"),
+                new SiswaModel("16669","Ahmad Nur Zaman",new DateTime(2006,2,3),"Laki-laki","Jl.Suko Mbahmu No.4,Bantul","TKR"),
+                new SiswaModel("16667","Beni Setyawan",new DateTime(2006,2,3),"Laki-laki","Jl.Yossudarso,Paten, Bantul","DKV"),
+                new SiswaModel("16663","Mela Meliani",new DateTime(2006,2,3),"Perempuan","Jl.Mawar No.57,Sleman","TKJ"),
+                new SiswaModel("16665","Yoga Setia Budi",new DateTime(2006,2,3),"Laki-laki","Jl.SukaMaju No.1 ,Bantul","AKL"),
+            };
+
+            bindingSource = new BindingSource();
+            bindingSource.DataSource = listSiswa;
+
+            comboBox1.Items.Add("Laki-laki");
+            comboBox1.Items.Add("Perempuan");
+
+            comboBox2.Items.Add("RPL");
+            comboBox2.Items.Add("TKJ");
+            comboBox2.Items.Add("TKR");
+            comboBox2.Items.Add("DKV");
+            comboBox2.Items.Add("AKL");
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = bindingSource;
+            dataGridView1.Columns["NIS"].Width = 50;
+            dataGridView1.Columns["Nama"].Width = 150;
+            dataGridView1.Columns["TglLahir"].Width = 100;
+            dataGridView1.Columns["Gender"].Width = 100;
+            dataGridView1.Columns["Alamat"].Width = 150;
+            dataGridView1.Columns["Jurusan"].Width = 100;
+            dataGridView1.Columns["TglLahir"].DefaultCellStyle.Format = "dd-MM-yyyy";
+        }
+
+        private void dataGridView1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listSiswa.FirstOrDefault(x => x.NIS == textBox1.Text).Gender = comboBox1.Text;
+            dataGridView1.Refresh();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var nis = dataGridView1.CurrentRow.Cells["NIS"].Value.ToString();
+            var siswa = listSiswa.FirstOrDefault(x => x.NIS == nis);
+
+            textBox1.Text = siswa.NIS;
+            textBox2.Text = siswa.Nama;
+            dateTimePicker1.Value = siswa.TglLahir;
+            comboBox1.Text = siswa.Gender;
+            textBox3.Text = siswa.Alamat;
+            comboBox2.Text = siswa.Jurusan;
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBox2_Validated(object sender, EventArgs e)
+        {
+            listSiswa.FirstOrDefault(x => x.NIS == textBox1.Text).Nama = textBox2.Text;
+            dataGridView1.Refresh();
+        }
+
+        private void textBox3_Validated(object sender, EventArgs e)
+        {
+            listSiswa.FirstOrDefault(x => x.NIS == textBox1.Text).Alamat = textBox3.Text;
+            dataGridView1.Refresh();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            listSiswa.FirstOrDefault(x => x.NIS == textBox1.Text).TglLahir = dateTimePicker1.Value;
+            dataGridView1.Refresh();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listSiswa.FirstOrDefault(x => x.NIS == textBox1.Text).Jurusan = comboBox2.Text;
+            dataGridView1.Refresh();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.CustomFormat = "dddd, dd-MM-yyyy";
+        }
+    }
+}
